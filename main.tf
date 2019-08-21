055508951908ac5f6c538bcf16991013a2b10090
junipernineaj <anthony@junipernine.com>
2019-08-21 16:05:38 +0100

    Add Security Group to the VPC Security Group IDs

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_security_group" "instance" {
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
